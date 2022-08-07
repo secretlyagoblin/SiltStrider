3032ce443b89bc5cab74f9bd27d96d6de31351b0
Chris Welch <chris.welch@aurecongroup.com>
2022-08-07 21:46:08 +1200

    Dropped in a chunk of code from MorroGen to implement

--- SiltStrider/Records/Landscape.cs
+++ SiltStrider/Records/Landscape.cs
@@ -21,6 +21,8 @@ namespace SiltStrider.Records
         public Landscape(long cellX, long cellY, IEnumerable<int> heights)
             : base($"landscape::{cellX}x{cellY}",new Long2(cellX,cellY))
         {
+            if (heights.Count() != 65 * 65) throw new System.Exception($"A Cell must contain exactly {65 * 65} cells.");
+
             int verticalDatum = heights.Min();
             VerticalDatum = verticalDatum;
 
@@ -40,7 +42,46 @@ namespace SiltStrider.Records
                     ColourMap[x, y] = new Byte3(0, 0, 0);
                 }
             }
-  
+
+            // Below from MorrowGen - needs to be implemented 
+
+            //  // Seems like for (i, j) j = 64 we have an differential offset vector that applies to a row at a time.
+            //  // SO height(i, j) = float offset + sum( height(n, 64), n = 0..i) ) + sum( height(i, n), n = 0..j) )
+            //  // So we have a 65x65 height map where row (i, 0) = float offset + sum( height(n, 64), n = 0..i )
+            //  //
+            //  // North is along increasing i, and so East is along increasing j.
+            //  int LandRecord::convertHeightMapToDiff()
+            //  {
+            //      signed char temp[65][65];
+            //      memset(temp, 0, 65 * 65 * sizeof(char));
+            //  
+            //      // point (0, 0) = float offset, soooo....
+            //      this->Unknown1 = round(this->AbsHeightMap[0][0]);
+            //      std::cout << "offset: " << this->Unknown1 << std::endl;
+            //  
+            //      // Now set the first column using the differential row offset vector...
+            //      for (unsigned int i = 1; i < 65; i++)
+            //      {
+            //          temp[i - 1][64] = this->AbsHeightMap[i][0] - this->AbsHeightMap[i - 1][0];
+            //      }
+            //  
+            //      // Now set all the other 64 columns using the differential row offset vector AND
+            //      // each points differential summation vector.
+            //      for (unsigned int i = 0; i < 65; i++)
+            //      {
+            //          for (unsigned int j = 0; j < 64; j++)
+            //          {
+            //              temp[i][j] = this->AbsHeightMap[i][j + 1] - this->AbsHeightMap[i][j];
+            //          }
+            //      }
+            //  
+            //      memcpy(&(this->DiffHeightMap), temp, 65 * 65);
+            //  
+            //      return 1;
+            //  }
+
+
+
         }
     }
 }
