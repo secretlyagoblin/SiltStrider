c6ca92f0083aa0cc51f2b630da7e04ae1e935ae9
Chris Welch <chris.welch@aurecongroup.com>
2022-08-13 00:47:49 +1200

    Implemented lands - no textures yet

--- SiltStrider/Records/Landscape.cs
+++ SiltStrider/Records/Landscape.cs
@@ -23,8 +23,8 @@ namespace SiltStrider.Records
         {
             if (heights.Count() != 65 * 65) throw new System.Exception($"A Cell must contain exactly {65 * 65} cells.");
 
-            int verticalDatum = heights.Min();
-            VerticalDatum = verticalDatum;
+            int verticalDatum = heights.First();
+           VerticalDatum = verticalDatum;
 
             var count = 0;
 
@@ -32,10 +32,9 @@ namespace SiltStrider.Records
             {
                 for (int y = 0; y < 65; y++)
                 {
-                    var potentialSbyte = heights.ElementAt(count++) - verticalDatum;
+                    var potentialSbyte = heights.ElementAt(count++);
 
-                    //here I need to really understand how the offsetting works -
-                    // https://en.uesp.net/morrow/tech/mw_esm.txt is kind of vague
+                    // https://en.uesp.net/morrow/tech/mw_esm.txt
 
                     Heightmap[x, y] = (sbyte)potentialSbyte;
                     Normalmap[x, y] = new SByte3(0, 0, 1);
@@ -43,45 +42,30 @@ namespace SiltStrider.Records
                 }
             }
 
-            // Below from MorrowGen - needs to be implemented 
+            var tempDiff = new sbyte[65, 65];
 
-            //  // Seems like for (i, j) j = 64 we have an differential offset vector that applies to a row at a time.
-            //  // SO height(i, j) = float offset + sum( height(n, 64), n = 0..i) ) + sum( height(i, n), n = 0..j) )
-            //  // So we have a 65x65 height map where row (i, 0) = float offset + sum( height(n, 64), n = 0..i )
-            //  //
-            //  // North is along increasing i, and so East is along increasing j.
-            //  int LandRecord::convertHeightMapToDiff()
-            //  {
-            //      signed char temp[65][65];
-            //      memset(temp, 0, 65 * 65 * sizeof(char));
-            //  
-            //      // point (0, 0) = float offset, soooo....
-            //      this->Unknown1 = round(this->AbsHeightMap[0][0]);
-            //      std::cout << "offset: " << this->Unknown1 << std::endl;
-            //  
-            //      // Now set the first column using the differential row offset vector...
-            //      for (unsigned int i = 1; i < 65; i++)
-            //      {
-            //          temp[i - 1][64] = this->AbsHeightMap[i][0] - this->AbsHeightMap[i - 1][0];
-            //      }
-            //  
-            //      // Now set all the other 64 columns using the differential row offset vector AND
-            //      // each points differential summation vector.
-            //      for (unsigned int i = 0; i < 65; i++)
-            //      {
-            //          for (unsigned int j = 0; j < 64; j++)
-            //          {
-            //              temp[i][j] = this->AbsHeightMap[i][j + 1] - this->AbsHeightMap[i][j];
-            //          }
-            //      }
-            //  
-            //      memcpy(&(this->DiffHeightMap), temp, 65 * 65);
-            //  
-            //      return 1;
-            //  }
+            //Solved with lots of help from https://bitbucket.org/auralgeek/morrogen LandRecord.cpp
 
+            for (int x = 1; x < 65; x++)
+            {
+                tempDiff[x - 1,64] = (sbyte)(Heightmap[x,0] - Heightmap[x - 1,0]);
+            }
 
+            for (int x = 0; x < 65; x++)
+            {
+                for (int y = 0; y < 64; y++)
+                {
+                    tempDiff[x,y] = (sbyte)(Heightmap[x,y + 1] - Heightmap[x,y]);
+                }
+            }
 
+            for (int x = 0; x < 65; x++)
+            {
+                for (int y = 0; y < 65; y++)
+                {
+                    Heightmap[x, y] = tempDiff[x, y];
+                }
+            }
         }
     }
 }
